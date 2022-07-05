006bfebe327997763ee09a3d7e828feeb598a9cd
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-07-05 19:44:07 +0300

    commit lombok(login)

--- src/test/java/testsApi/UserApiTest.java
+++ src/test/java/testsApi/UserApiTest.java
@@ -3,20 +3,25 @@ package testsApi;
 import com.github.javafaker.Faker;
 import configuration.Endpoints;
 import helpers.UserHelper;
+import io.restassured.specification.Argument;
 import models.UserApi;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class UserApiTest extends BaseApiTest {
 
     public UserHelper userHelper = new UserHelper();
     public UserApi user, newUser;
+    private int userId;
     Faker faker = new Faker();
 
     @Test
@@ -33,7 +38,19 @@ public class UserApiTest extends BaseApiTest {
         jsonAsMap.put("email", faker.internet().emailAddress());
         jsonAsMap.put("roleId", 1);
 
-        user = userHelper.addUser(jsonAsMap);
+        newUser = given()
+                .body(jsonAsMap)
+                .when()
+                .post(Endpoints.ADD_USER)
+                .then()
+                .statusCode(org.apache.hc.core5.http.HttpStatus.SC_OK)
+                .log().body()
+                .extract()
+                .jsonPath().get("id");
+
+        userId = newUser.getId();
+               // .as(UserApi.class);
+      //  user = userHelper.addUser(jsonAsMap);
     }
 
     @Test
@@ -42,14 +59,16 @@ public class UserApiTest extends BaseApiTest {
 
         given()
                 .when()
-                .pathParams("user_id", 29 )
+                .pathParams("user_id", userId )
                 .get(Endpoints.GET_USER)
                 .then()
                 .log().body()
+                .assertThat()
                 .statusCode(HttpStatus.SC_OK)
-                .extract()
-                .as(UserApi.class);
+                .body("id", is(29))
+                .body("name", equalTo(faker.name().fullName()))
+                .extract();
+                //.as(UserApi.class);
     }
-
 
 }
