91358dfaec0d0802b56f325f3d498e991d42d9c4
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-07-14 18:24:59 +0300

    со 2 вариантом апи

--- src/test/java/testsApi/UserApiTest.java
+++ src/test/java/testsApi/UserApiTest.java
@@ -1,75 +1,50 @@
 package testsApi;
 
 import com.github.javafaker.Faker;
-import configuration.Endpoints;
+import helpers.UserHelper;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Step;
-import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.mapper.ObjectMapperType;
-import io.restassured.path.json.JsonPath;
 import models.UserApiBuilder;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.http.HttpStatus;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 @Epic("Тестирование API user")
 public class UserApiTest extends BaseApiTest {
 
-    public UserApiBuilder user;
-    private JsonPath newUser;
+    private UserApiBuilder user, userId;
     Faker faker = new Faker();
+    UserHelper userHelper = new UserHelper();
 
     @Test(testName = "Запрос POST на создание пользователя")
     @Feature("Создание пользователя")
     @Step("Пользователь с ролью Lead создан")
-    public void successAddUserTest() {
+    public void addUserTest() {
 
         user = UserApiBuilder.builder()
                 .name(faker.name().fullName())
                 .email(faker.internet().emailAddress())
                 .roleId(1)
                 .build();
+        String expected = "Lead";
 
-        newUser = given()
-                .body(user, ObjectMapperType.GSON)
-                .filter(new AllureRestAssured())
-                .when()
-                .post(Endpoints.ADD_USER)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .log().body()
-                .body("role", equalTo("Lead"))
-                .extract().jsonPath();
-
+        userId = userHelper.addUser(user, expected);
     }
 
-    @Test(testName = "Запрос POST на редактирование роли пользователя", dependsOnMethods = "successAddUserTest")
+    @Test(testName = "Запрос POST на редактирование роли пользователя", dependsOnMethods = "addUserTest")
     @Feature("Редактирование роли пользователя")
     @Step("Роль пользователя изменена на Tester")
+    @Parameters
     public void successUpdateUserTest() {
 
         user = UserApiBuilder.builder()
-                .id(newUser.getInt("id"))
+                .id(userId.getId())
                 .roleId(3)
                 .build();
+        String expected = "Tester";
 
-        given()
-                .body(user, ObjectMapperType.GSON)
-                .filter(new AllureRestAssured())
-                .when()
-                .post(Endpoints.UPDATE_USER)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .log().body()
-                .body("role", equalTo("Tester"))
-                .extract().jsonPath();
+        userHelper.updateUser(user, expected);
     }
 
     @Test(testName = "Отправка запроса GET на зачитку данных пользователя", dependsOnMethods = "successUpdateUserTest")
@@ -77,78 +52,67 @@ public class UserApiTest extends BaseApiTest {
     @Step("Пользователь успешно зачитан")
     public void successGetUserTest() {
 
-        given()
-                .pathParams("user_id", newUser.getInt("id"))
-                .filter(new AllureRestAssured())
-                .when()
-                .get(Endpoints.GET_USER)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .log().body()
-                .body("id", is(newUser.getInt("id")))
-                .body("name", equalTo(newUser.getString("name")))
-                .body("role", equalTo("Tester"))
-                .extract();
+        UserApiBuilder expected = UserApiBuilder.builder()
+                .role("Tester")
+                .name(userId.getName())
+                .email(userId.getEmail())
+                .id(userId.getId())
+                .build();
+
+        userHelper.getUser(userId, expected);
+    }
+
+    @Test(testName = "Отправка запроса GET на зачитку данных пользователя по email", dependsOnMethods = "successGetUserTest")
+    @Feature("Получение данных пользователя по email")
+    @Step("Пользователь успешно зачитан по email")
+    public void successGetUserByEmailTest() {
+
+        UserApiBuilder expected = UserApiBuilder.builder()
+                .role("Tester")
+                .name(userId.getName())
+                .email(userId.getEmail())
+                .id(userId.getId())
+                .build();
+
+        userHelper.getUserByEmail(userId, expected);
     }
 
     @Test(testName = "Получение ошибки на зачитку пользователя с несуществующим ID")
     @Feature("Валидация на зачитку пользователя с несуществующим ID")
     @Step("Получена ошибка при зачитке пользователя с несуществующим ID")
-    public void failGetUserWithoutIdTest2() {
+    public void failGetUserWithIdNotFoundTest() {
 
-        given()
-                .pathParams("user_id", 0)
-                .filter(new AllureRestAssured())
-                .when()
-                .get(Endpoints.GET_USER)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .log().body()
-                .body("error", equalTo("Field :user is not a valid user."))
-                .extract();
+        user = UserApiBuilder.builder()
+                .id(0)
+                .build();
+
+        String expected = "Field :user is not a valid user.";
+        userHelper.getUserErrorInt(user, expected);
     }
 
     @Test(testName = "Получение ошибки на зачитку пользователя без ID")
     @Feature("Валидация на зачитку пользователя без указания ID")
     @Step("Получена ошибка при зачитке пользователя без ID")
     public void failGetUserWithoutIdTest() {
+        String user = "";
+        String expected = "Field :user is a required field.";
 
-        given()
-                .pathParams("user_id", "")
-                .filter(new AllureRestAssured())
-                .when()
-                .get(Endpoints.GET_USER)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .log().body()
-                .body("error", equalTo("Field :user is a required field."))
-                .extract();
+        userHelper.getUserErrorString(user, expected);
     }
 
     @Test(testName = "Получение ошибки на зачитку пользователя с некорректным ID")
     @Feature("Валидация на зачитку пользователя с некорректным ID")
     @Step("Получена ошибка при зачитке пользователя с некорректным ID")
     public void failGetUserWithIIncorrectIdTest() {
+        String user = "incorrect";
+        String expected = "Field :user is not a valid ID.";
 
-        given()
-                .pathParams("user_id", "incorrect")
-                .filter(new AllureRestAssured())
-                .when()
-                .get(Endpoints.GET_USER)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .log().body()
-                .body("error", equalTo("Field :user is not a valid ID."))
-                .extract();
+        userHelper.getUserErrorString(user, expected);
     }
 
     @Test(testName = "Получение ошибки на создание пользователя с именем, превышающим допустимый размер поля")
     @Feature("Валидация на создание пользователя с именем > 250 символов")
-    @Step("Подучена ошибка на создание пользователя с именем > 250 символов")
+    @Step("Получена ошибка на создание пользователя с именем > 250 символов")
     public void failAddUserNameToLongTest() {
         String generatedString = RandomStringUtils.randomAlphabetic(251);
         user = UserApiBuilder.builder()
@@ -156,16 +120,8 @@ public class UserApiTest extends BaseApiTest {
                 .email(faker.internet().emailAddress())
                 .build();
 
-        given()
-                .body(user)
-                .filter(new AllureRestAssured())
-                .when()
-                .post(Endpoints.ADD_USER)
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .log().body()
-                .body("error", equalTo("Field :name is too long (250 characters at most)."))
-                .extract();
+        String expected = "Field :name is too long (250 characters at most).";
+        userHelper.addUserError(user, expected);
     }
+
 }
