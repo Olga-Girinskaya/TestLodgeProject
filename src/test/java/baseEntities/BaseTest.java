e7be2bd882dd4982b78df4959d980ec359f817a8
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-07-16 11:36:03 +0300

    commit

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -18,7 +18,6 @@ public class BaseTest {
     public static WebDriver driver;
     protected LoginStep loginStep;
     protected TestCaseStep testCaseStep;
-    protected DashboardStep dashboardStep;
     protected AllTestCasePage allTestCasePage;
 
     @BeforeMethod
@@ -26,7 +25,6 @@ public class BaseTest {
         driver = new BrowsersService().getDriver();
         loginStep = new LoginStep(driver);
         testCaseStep = new TestCaseStep(driver);
-        dashboardStep = new DashboardStep(driver);
         allTestCasePage = new AllTestCasePage(driver);
 
         driver.get(ReadProperties.getUrl());
