7c26b1bafc4b1089be6ffa47085f4509d165a82a
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-07-16 11:30:14 +0300

    commit

--- src/test/java/steps/TestCaseStep.java
+++ src/test/java/steps/TestCaseStep.java
@@ -4,7 +4,6 @@ import baseEntities.BaseStep;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Test;
 import page.AllTestCasePage;
 import page.DashboardPage;
 import page.TestCasePage;
