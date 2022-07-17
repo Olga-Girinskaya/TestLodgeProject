a8811ad1cd350ad88f13e17b69cec8f26bec6b2c
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-07-17 10:26:30 +0300

    commit

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -15,7 +15,7 @@ import steps.TestCaseStep;
 
 
 public class BaseTest {
-    public static WebDriver driver;
+    public  WebDriver driver;
     protected LoginStep loginStep;
     protected TestCaseStep testCaseStep;
     protected AllTestCasePage allTestCasePage;
